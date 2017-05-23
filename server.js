c4d70b7da93ed91beb8a124d42782b03427da674
ColeTrammer <cole.trammer@gmail.com>
2017-05-23 16:13:02 -0700

    made ejs home page

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 "use strict";
 const express = require("express");
+const Mongo = require("mongodb").MongoClient;
+const path = require("path");
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Connected to port ${process.env.PORT}.`)
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`Connected to port ${app.get("port")}.`);
 });
